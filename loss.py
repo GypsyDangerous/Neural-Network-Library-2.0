a03967f1c97d0845668381c67a21ca96ac5720e3
David Snyder <45735850+GypsyDangerous@users.noreply.github.com>
2019-03-27 11:40:12 -0400

    Update loss.py

--- loss.py
+++ loss.py
@@ -5,7 +5,7 @@ a helper module for loss functions, more are going to be added.
 '''
 
 def cross_entropy(targets, guess):
-	return np.sum(targets * -np.log(guess))
+	return np.sum(targets * (-np.log(guess)))
 
 def cross_entropy_p(targets, guess):
 	return -np.sum(targets / guess)
@@ -17,11 +17,3 @@ def mse(targets, guess):
 def mse_p(targets, guess):
 	return guess - targets
 
-
-if __name__ == "__main__":
-	test = np.array([0, 0, 0, 1, 0, 0])
-	other = np.array([0.01, 0, 0, 0.9, 0, 0])
-	test = test.reshape(6, 1)
-	other = other.reshape(6, 1)
-	print(other)
-	print(cross_entropy(test, other))
