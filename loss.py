ffeb9b8fb6fed79820fd1a972a07c01736fe70c6
David Snyder <45735850+GypsyDangerous@users.noreply.github.com>
2019-03-27 11:41:12 -0400

    Update loss.py

--- loss.py
+++ loss.py
@@ -5,6 +5,9 @@ a helper module for loss functions, more are going to be added.
 '''
 
 def cross_entropy(targets, guess):
+	'''
+	targets is a one hot encoded vector and guess is the output vector of the network
+	'''
 	return np.sum(targets * (-np.log(guess)))
 
 def cross_entropy_p(targets, guess):
@@ -12,7 +15,7 @@ def cross_entropy_p(targets, guess):
 
 
 def mse(targets, guess):
-		return np.mean((targets-guess)**2)
+	return np.mean((targets-guess)**2)
 
 def mse_p(targets, guess):
 	return guess - targets
