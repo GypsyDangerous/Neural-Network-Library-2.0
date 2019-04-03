3588c662491abfcb9afef14395e0ffbebfce966f
David Snyder <45735850+GypsyDangerous@users.noreply.github.com>
2019-04-03 16:00:05 -0400

    Update NeuralNetWork2_0.py

--- NeuralNetWork2_0.py
+++ NeuralNetWork2_0.py
@@ -3,15 +3,9 @@ import time
 import os
 from Layer import *
 from loss import *
+from misc import *
 
-'''
-helper functions to display the accuracy of the network as a percentage with two decimals
-'''
-def truncate(x, level=100):
-	return int(x*level)/level
 
-def percent(x, total=100, level=100):
-	return truncate((x/total)*100, level)
 
 class NeuralNetwork:
 
@@ -51,7 +45,7 @@ class NeuralNetwork:
 			else:
 				self.layers.append(layer(prevsize, size, acitivation))
 
- 	# functions to adjust the hyperparameters of the network
+	'''functions to adjust the hyperparameters of the network'''
 
 	def setLearningRate(self, val):
 		self.learning_rate = val
@@ -72,36 +66,61 @@ class NeuralNetwork:
 		self.epochs+=inc
 
 
-
-	# Work in Progress
-	def mutate(mr):
-		mutation_rate = mr
-
-
-
-	def fit(self, inputArray, labels):
+	def fit(self, trainingArray, labels, test_data=None, test_labels=None, test=False):
 
 		# error checking
-		if len(inputArray) > len(labels):
+		if len(trainingArray) > len(labels):
 			raise Exception("Your have more data points than labels")
 
-		if len(inputArray) < len(labels):
+		if len(trainingArray) < len(labels):
 			raise Exception("Your have more label points than data points")
+
 
 		'''
 		train the network over all the training data in a random order for self.epochs number 
 		of times
 		'''
-		datasize = len(inputArray)
+		datasize = len(trainingArray)
 		indices = np.arange(datasize)
+		start = time.time()
+		if test:
+			info = "Training, Testing mode: active"
+		else:
+			info = "Training, Testing mode: inactive"
+		print(info)
 		for i in range(self.epochs):
 			np.random.shuffle(indices)
 			for j in range(datasize):
 				index = indices[j]
-				info = inputArray[index]
-				goal = labels[index]
-				self.train(info, goal)
-	
+				info, goal = trainingArray[index], labels[index]
+
+				# self.process(info)
+
+				nabla_b = [np.zeros(self.layers[i].biases.shape) for i in range(self.size)]
+				nabla_w = [np.zeros(self.layers[i].weights.shape) for i in range(self.size)]
+
+				# delta_nabla_b, delta_nabla_w = self.backprop(goal)
+				# nabla_w += delta_nabla_w
+				# nabla_b += delta_nabla_b
+
+				# for i in range(len(nabla_w)):
+				# 	self.layers[i].update_weights(nabla_w[i], self.learning_rate)
+
+				# for i in range(len(nabla_b)):
+				# 	self.layers[i].update_biases(nabla_b[i], self.learning_rate)
+
+
+			Epoch = i+1
+
+			if not test:
+				print("epoch: %d, %a%% finished" % (Epoch, percent(Epoch, self.epochs)))
+			else:
+				print("epoch: %d, Accuracy: %a%%, %a%% finished" % 
+					(Epoch, self.test(test_data, test_labels), percent(Epoch, self.epochs)))
+
+		end = time.time();
+		print("Training complete, time passed:", time_format(end-start), )
+
 
 	def process(self, inputs):
 		'''
@@ -109,22 +128,21 @@ class NeuralNetwork:
 		'''
 		for i in range(self.size-1):
 			if i == 0:
-				inputlayer = self.layers[0]
-				inputlayer.values = np.array(inputs).reshape(inputlayer.nodes, 1)
+				self.layers[0].input_layer(inputs)
 				
 			self.layers[i].feedforward(self.layers[i+1]) # see layers class feedforward function
-		self.outputs = self.layers[-1].values
+		self.outputs = self.layers[-1].activations
 		return self.outputs
 
 
-	def test(self, test_data, test_labels):
+	def test(self, test_data, test_labels, Print=False):
 
 		# error checking
 		if len(test_data) > len(test_labels):
-			raise Exception("Your have more test data points than labels")
+			raise ValueError("Your have more test data points than labels")
 
 		if len(test_data) < len(test_labels):
-			raise Exception("Your have more test labels than data points")
+			raise ValueError("Your have more test labels than data points")
 
 
 		'''
@@ -140,17 +158,45 @@ class NeuralNetwork:
 			guess = self.process(data)
 			Guess = np.argmax(guess)
 			Label = np.argmax(label)
-			error = self.loss(label, guess)
-			confidence = percent(guess[Guess], 1, 100)
+		
 			verdict = ""
 			if Guess == Label:
 				acc += 1
 				verdict = "correct"
 			else:
 				verdict = "incorrect"
-			print(i, " error: %f, guess: %d, answer: %d, confidence: %a%%, verdict %s" % (error, Guess, Label, confidence, verdict)) # optional line for debugging
+			if Print:
+				error = self.loss(label, guess)
+				confidence = percent(guess[Guess], 1, 100)
+				print(i, " error: %f, guess: %d, answer: %d, confidence: %a%%, verdict %s" % (error, Guess, Label, confidence, verdict)) # optional line for debugging
+
 		self.accuracy = percent(acc, datalen)
-		print("accuracy: %a%%" %(self.accuracy))
+		if Print:
+			print("accuracy: %a%%" % (self.accuracy))
+		return self.accuracy
+
+
+
+	def backprop(self, targets):
+		'''
+		backpropagation function im trying to make, there is a bug somewhere that I can't find, 
+		it is causing matrix size errors in the dot product
+		'''
+		nabla_b = [np.zeros(self.layers[i].biases.shape) for i in range(self.size)]
+		nabla_w = [np.zeros(self.layers[i].weights.shape) for i in range(self.size)]
+
+
+		error = (self.loss_p(targets, self.outputs) * self.layers[-1].activation_p(self.layers[-1].outputs))
+
+		nabla_b[-1] = error
+		nabla_w[-1] = error.dot(self.layers[-2].activations.T)
+
+		for i in range(self.size - 2, 1, -1):
+			error = self.layers[i+1].weights.T.dot(error) * self.layers[i].activation_p(self.layers[i].outputs)
+			nabla_b[i] = error
+			nabla_w[i] = error.dot(self.layers[i-1].activations.T)
+
+		return nabla_b, nabla_w
 
 
 	def save(self, filename='2model.npz'):
@@ -184,36 +230,6 @@ class NeuralNetwork:
 		self.epochs = int(npz_members['epochs'])
 
 	
-	def train(self, inputs, targets):
-		'''
-		attempting to implement backpropagation with gradient descent. I need help with this part
-		'''
-		targets = np.array(targets).reshape(self.layers[-1].nodes, 1)
-		guess = self.process(inputs)
-
-		for i in range(self.size-1, 0, -1):
-			
-			outputs = self.layers[i].values
-			error = self.loss_p(targets, outputs)
-
-			gradient = self.layers[i].activation_p(outputs)
-			gradient *= error
-			gradient *= self.learning_rate
-
-
-			modifiying = self.layers[i]
-
-			delta = np.dot(gradient, modifiying.values.T)
-
-			modifiying.biases += gradient
-			modifiying.weights += delta
-
-			temp = modifiying.weights.T
-			temp = np.dot(temp, error)
-			temp += modifiying.values
-			targets = temp
-			
-
 
 
 
