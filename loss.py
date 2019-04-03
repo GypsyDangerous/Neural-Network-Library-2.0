c1ff533960368a05ab2ec7d097ec0c78175b668c
David Snyder <45735850+GypsyDangerous@users.noreply.github.com>
2019-04-03 16:02:58 -0400

    Update loss.py

--- loss.py
+++ loss.py
@@ -1,21 +1,20 @@
 import numpy as np
+from activations import *
 
 '''
 a helper module for loss functions, more are going to be added.
 '''
 
 def cross_entropy(targets, guess):
-	'''
-	targets is a one hot encoded vector and guess is the output vector of the network
-	'''
-	return np.sum(targets * (-np.log(guess)))
+	return np.sum(targets * -np.log(guess))
 
+# I don't know if this is correct but it is what I got from https://github.com/mnielsen/neural-networks-and-deep-learning/blob/master/src/network2.py
 def cross_entropy_p(targets, guess):
-	return -np.sum(targets / guess)
+	return guess - targets 
 
 
 def mse(targets, guess):
-	return np.mean((targets-guess)**2)
+		return np.mean((targets-guess)**2)
 
 def mse_p(targets, guess):
 	return guess - targets
